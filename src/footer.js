09e3ad41e5782f5a5a2c06028c5ebb7b84bf77fb
trevorstam <trevorstam@gmail.com>
2018-11-27 15:32:34 -0800

    react app done and styled

---
+++ src/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render(){
+    return(
+      <React.Fragment>
+        <footer>
+          <p>2K18</p>
+        </footer>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render(){
+    return(
+      <React.Fragment>
+        <main>
+          <span>React is my Jam y'all</span>
+          < img src = "https://media.giphy.com/media/3lxD1O74siiz5FvrJs/giphy.gif" alt="my jam"/>
+        </main>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Main;
